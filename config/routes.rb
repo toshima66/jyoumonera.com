5a9ef8e134c8ae26f9c8b6d9c383ade4368f70c3
toshima66 <toshima66@6vox.com>
2014-07-19 15:44:28 +0900

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ JyoumoneraCom::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'index.html'
+  # root 'index.html'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
